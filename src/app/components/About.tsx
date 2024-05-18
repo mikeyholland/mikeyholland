e319e36e77601fd878fcac4936b73782d04cd0ea
Mikey Holland <mpjholland@gmail.com>
2024-05-18 22:10:14 +0100

    Initial commit - setup dev config, fonts, and colors

---
+++ src/app/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <header>
+      <h1 className="font-bold italic">Mikey Holland</h1>
+      <h2>Freelance Frontend Developer</h2>
+    </header>
+  );
+}
+
+export default About;
