9cc56d6f65a5459e804ce07eadca5026f182ac74
Mikey Holland <mpjholland@gmail.com>
2024-07-08 16:33:05 +0100

    Remove log from lib/image

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -6,6 +6,5 @@ import { client } from '../client';
 const imageBuilder = imageUrlBuilder(client);
 
 export const urlForImage = (source: Image) => {
-  console.log({ source });
   return imageBuilder?.image(source).auto('format').fit('max').url();
 };
