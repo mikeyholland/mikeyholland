eda69d39f471867a0fcf297ef94bc66724dbe3d2
Mikey Holland <mpjholland@gmail.com>
2024-07-18 23:27:33 +0100

    Change in view ref in Section to span element

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -18,8 +18,8 @@ export const Section = forwardRef<HTMLElement, SectionType>(
   ({ className, children, bgColor, textColor }, ref) => {
     const { setBgColor } = useContext(BgContext);
     const { setTextColor } = useContext(TextContext);
-    const bgRef = useRef<HTMLDivElement>(null);
-    const isInView = useInView(bgRef);
+    const inViewRef = useRef<HTMLDivElement>(null);
+    const isInView = useInView(inViewRef);
 
     useEffect(() => {
       if (isInView) {
@@ -33,9 +33,8 @@ export const Section = forwardRef<HTMLElement, SectionType>(
         ref={ref}
         className="w-full flex items-center md:min-h-screen first:mt-0 px-4 py-6 my-56 md:px-8 md:my-96 md:first:py-24 last:md:py-0 last:mb-0"
       >
-        <div ref={bgRef} className={clsx('', className)}>
-          {children}
-        </div>
+        <span ref={inViewRef} />
+        <div className={clsx('', className)}>{children}</div>
       </section>
     );
   },
